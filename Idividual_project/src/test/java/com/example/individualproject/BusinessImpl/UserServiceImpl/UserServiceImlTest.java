485e71f14edc8ad10272af4e9bd42ee3f97cc64d
lars <l.kluijtmans@student.fontys.nl>
2022-04-12 23:10:03 +0200

    Got many problem with the git when I pulled the old project it won't open and only showed the gitignore file

---
+++ Idividual_project/src/test/java/com/example/individualproject/BusinessImpl/UserServiceImpl/UserServiceImlTest.java
@@ -0,0 +1,162 @@
+package com.example.individualproject.BusinessImpl.UserServiceImpl;
+
+
+import com.example.individualproject.DTO.Users.CreateUserRequestDTO;
+import com.example.individualproject.DTO.Users.GetUserDTO;
+import com.example.individualproject.DTO.Users.UpdateUserRequestDTO;
+import com.example.individualproject.DTO.Users.UserAccountRequestDTO;
+import com.example.individualproject.business.impl.UserServiceImpl;
+import com.example.individualproject.repository.AdminRepository;
+import com.example.individualproject.repository.NormalUserRepository;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class UserServiceImlTest {
+
+    @Mock
+    private NormalUserRepository normalUserRepository;
+
+    @Mock
+    private AdminRepository adminRepository;
+
+    @InjectMocks
+    private UserServiceImpl userService;
+
+    @Test
+    void test_GetAllUsers() {
+        List<GetUserDTO> result = userService.getAllUsers();
+
+        assertEquals(0, result.size());
+    }
+
+    @Test
+    void test_GetAllNormalUsers() {
+        CreateUserRequestDTO createUserRequestDTO = new CreateUserRequestDTO("lars","lars","lars","lars","lars","lars");
+
+        userService.addUser(createUserRequestDTO);
+
+        List<GetUserDTO> result = userService.getAllUsers();
+
+        assertEquals(1, result.size());
+    }
+
+    @Test
+    void test_GetAllAdmin() {
+        CreateUserRequestDTO createUserRequestDTO = new CreateUserRequestDTO("lars","lars","lars","lars","lars","lars");
+
+        userService.addUser(createUserRequestDTO);
+
+        List<GetUserDTO> result = userService.getAllAdmins();
+
+        assertEquals(0, result.size());
+    }
+
+    @Test
+    void test_GetUserByID() {
+        CreateUserRequestDTO createUserRequestDTO = new CreateUserRequestDTO("lars","lars","lars","lars","lars","lars");
+
+        userService.addUser(createUserRequestDTO);
+
+        GetUserDTO result = userService.getUserByID(1l);
+
+        assertEquals("lars", result.getUsername());
+        assertEquals("lars", result.getFirstName());
+        assertEquals("lars", result.getLastName());
+        assertEquals("lars", result.getPhoneNumber());
+        assertEquals("lars", result.getEmail());
+    }
+
+    @Test
+    void test_GetAllUserByName() {
+        CreateUserRequestDTO createUserRequestDTO = new CreateUserRequestDTO("lars","lars","lars","lars","lars","lars");
+
+        userService.addUser(createUserRequestDTO);
+
+        List<GetUserDTO> result = userService.getAllUserByName("lars");
+
+        assertEquals(1, result.size());
+
+
+    }
+
+    @Test
+    void test_DeleteUser() {
+        CreateUserRequestDTO createUserRequestDTO = new CreateUserRequestDTO("lars","lars","lars","lars","lars","lars");
+
+        userService.addUser(createUserRequestDTO);
+
+        List<GetUserDTO> result = userService.getAllUsers();
+
+        assertEquals(1, result.size());
+
+        userService.deleteUser(1l);
+
+        result = userService.getAllUsers();
+
+        assertEquals(0, result.size());
+
+    }
+
+    @Test
+    void test_GetUserByAccount() {
+        CreateUserRequestDTO createUserRequestDTO = new CreateUserRequestDTO("lars","lars","lars","lars","lars","lars");
+
+        userService.addUser(createUserRequestDTO);
+
+        GetUserDTO result = userService.getUser( new UserAccountRequestDTO("lars", "lars"));
+
+        assertEquals("lars", result.getUsername());
+    }
+
+    @Test
+    void test_AddUser() {
+        CreateUserRequestDTO createUserRequestDTO = new CreateUserRequestDTO("lars","lars","lars","lars","lars","lars");
+
+        userService.addUser(createUserRequestDTO);
+
+        List<GetUserDTO> result = userService.getAllUsers();
+
+        assertEquals(1, result.size());
+
+         createUserRequestDTO = new CreateUserRequestDTO("lars2","lars","lars","lars","lars2","lars2");
+
+        userService.addUser(createUserRequestDTO);
+
+         result = userService.getAllUsers();
+
+        assertEquals(2, result.size());
+    }
+
+    @Test
+    void test_UpdateUser() {
+        CreateUserRequestDTO createUserRequestDTO = new CreateUserRequestDTO("lars","lars","lars","lars","lars","lars");
+
+        userService.addUser(createUserRequestDTO);
+
+        GetUserDTO result = userService.getUserByID(1l);
+
+        assertEquals("lars", result.getUsername());
+        assertEquals("lars", result.getFirstName());
+        assertEquals("lars", result.getLastName());
+        assertEquals("lars", result.getPhoneNumber());
+        assertEquals("lars", result.getEmail());
+
+        UpdateUserRequestDTO updateUserRequestDTO = new UpdateUserRequestDTO(1l,"lars2","lars2","lars2","lars2");
+
+        userService.updateUser(updateUserRequestDTO);
+
+        result = userService.getUserByID(1l);
+
+        assertEquals("lars", result.getUsername());
+        assertEquals("lars2", result.getFirstName());
+        assertEquals("lars2", result.getLastName());
+        assertEquals("lars2", result.getPhoneNumber());
+        assertEquals("lars2", result.getEmail());
+    }
+}
+
